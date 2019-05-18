b7149e04e1c825fd701ebae647d2c107a6d4394b
maciej <sucheckimaciej3@gmail.com>
2019-05-18 19:39:55 +0200

    Writing files with unused methods

---
+++ src/main/java/com/sda/fileoperation/FilesHomeTask3/factory/StudentRandomFactory.java
@@ -0,0 +1,26 @@
+package com.sda.fileoperation.FilesHomeTask3.factory;
+
+import com.sda.fileoperation.FilesHomeTask3.Student;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class StudentRandomFactory {
+    public List<Student> randomizeStudents(List<String> names, List<String> surnames, int numberOfStudents, int randomizeFactor) {
+        List<Student> resultList = new LinkedList<Student>();
+
+        Random rand = new Random();
+        rand.setSeed(System.currentTimeMillis()*randomizeFactor);
+
+        int nameIndex, surnameIndex;
+
+        for (int i = 0; i < numberOfStudents; i++) {
+            nameIndex = rand.nextInt(names.size());
+            surnameIndex = rand.nextInt(surnames.size());
+            resultList.add(new Student(names.get(nameIndex), surnames.get(surnameIndex), rand.nextInt(23) + 18));
+        }
+        return resultList;
+    }
+
+}
